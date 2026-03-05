4004021b81ac4f484b7f28eefbccda674cc6d32e
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-05 10:32:28 +0500

    feat: add login controller with password verification and jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,3 +51,20 @@ export const registerUser = async (req, res) => {
       .json({ success: false, message: "Internal server error" });
   }
 };
+
+// login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please fill all the fields" });
+  }
+  try {
+    const user = await User.findOne({ email });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
