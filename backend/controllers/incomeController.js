e3a4a2020cf38fbdd1772a44c6a7492edf16e358
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-06 16:13:42 +0500

    feat: add income controller and addIncome API

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,38 @@
+import IncomeModel from "../models/incomeModel.js";
+
+export async function addIncome(req, res) {
+  const userId = req.user._id;
+  const { description, amount, category, date } = req.body;
+
+  try {
+    if (!description || !amount || !category || !date) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const newIncome = new IncomeModel({
+      userId,
+      description,
+      amount,
+      category,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Income added successfully",
+      income: newIncome,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+}
