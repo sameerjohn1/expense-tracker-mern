a2f626d7d1c86496c2189d407c990516787c4000
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-09 10:33:10 +0500

    Add deleteIncome controller to delete income by id

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -84,5 +84,36 @@ export async function updateIncome(req, res) {
       message: "Income updated successfully",
       data: updatedIncome,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+}
+
+// to delete an income
+export async function deleteIncome(req, res) {
+  try {
+    const income = await IncomeModel.findByIdAndDelete({ _id: req.params.id });
+
+    if (!income) {
+      return res.status(404).json({
+        success: false,
+        message: "Income not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Income deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
 }
